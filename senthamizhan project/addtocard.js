997f436c66a2f5bc17ae0fe593401ea0878aefb9
cochengannan <cochengannanofficial@gmail.com>
2025-03-04 11:47:24 +0530

    Add files via upload

---
+++ senthamizhan project/addtocard.js
@@ -0,0 +1,11 @@
+
+        <script>
+            function BuyNow()
+            {
+                
+                let Adder=prompt("Enter your address:");
+                let phone=prompt("Enter your phone number:");
+                let lndmark=prompt("Enter your near by landmark:");
+                
+            }
+        </script>
